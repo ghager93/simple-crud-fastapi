788c2306e44466e9918abb4c4f4eadec8e2e1730
ghager93 <ghager93@gmail.com>
2023-01-26 20:50:45 +1100

    Post skeleton.

--- app/api/simple_api.py
+++ app/api/simple_api.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from app.models import Simple
 
 router = APIRouter(prefix="/api")
 
@@ -7,3 +8,8 @@ router = APIRouter(prefix="/api")
 @router.get("/helloworld")
 async def hello_world():
     return {"hello world!"}
+
+
+@router.post("/simple/")
+async def create_simple(simple: Simple):
+    return simple
--- tests/test_app/api/test_simple_api.py
+++ tests/test_app/api/test_simple_api.py
@@ -1,9 +1,16 @@
 from fastapi.testclient import TestClient
 
+
 def test_hello_world(test_client: TestClient):
     """Test hello world endpoint returns correctly"""
     result = test_client.get("api/helloworld")
 
     assert result.status_code == 200
     assert result.json() == ["hello world!"]
+
+
+def test_post_returns_2xx(test_client: TestClient):
+    result = test_client.post("api/simple", json={})
+
+    assert 200 <= result.status_code < 300
 
