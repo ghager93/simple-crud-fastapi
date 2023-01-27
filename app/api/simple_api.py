914b766d6c6666bd9107a9da1651f075c605a353
ghager93 <ghager93@gmail.com>
2023-01-27 14:40:54 +1100

    Added Simple model to endpoint

--- app/api/simple_api.py
+++ app/api/simple_api.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from fastapi import APIRouter
 
-from app.models import SimpleCreate
+from app.models import SimpleCreate, Simple
 
 router = APIRouter(prefix="/api")
 
@@ -11,5 +12,12 @@ async def hello_world():
 
 
 @router.post("/simple/")
-async def create_simple(simple: SimpleCreate):
+async def create_simple(simple_create: SimpleCreate):
+    timestamp = datetime.now()
+    simple = Simple(
+        name=simple_create.name,
+        number=simple_create.number,
+        created_at=timestamp,
+        updated_at=timestamp
+    )
     return simple
